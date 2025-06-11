ca6cfb93a8ac9853362409a6551176ff49293f40
Ashok Sekar <ashoksekar@Ashoks-MacBook-Pro.local>
2025-06-11 14:10:39 +0100

    first commit

---
+++ first_project.sql
@@ -0,0 +1,9 @@
+SELECT * FROM activity.orders;
+
+
+Write a query to display all the details from the table order where the order amount is more than
+2000.
+
+select * 
+from orders
+where order_amount > 2000;  
